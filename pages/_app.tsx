d9c4da3d853ed29c4e8262df6631c79ee30de243
Lê Huy Vũ <vule@MacBook-Air-cua-Le.local>
2022-05-22 23:05:56 +0700

    Done 20% this project

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,17 @@
-import '../styles/globals.css'
+import { SessionProvider } from 'next-auth/react'
 import type { AppProps } from 'next/app'
+import Header from '../components/Header'
+import '../styles/globals.css'
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+function MyApp({ Component, pageProps: { session, ...pageProps } }: AppProps) {
+  return (
+    <SessionProvider session={session}>
+      <div className="h-screen overflow-y-scroll bg-slate-200">
+        <Header />
+      </div>
+      <Component {...pageProps} />
+    </SessionProvider>
+  )
 }
 
 export default MyApp
